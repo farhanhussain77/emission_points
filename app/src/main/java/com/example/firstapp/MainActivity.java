52b9c5d019575a9ff4df30c63872e9d9a9dac15b
Farhan Hussain <h.farhanhussain96@gmail.com>
2021-11-09 23:04:55 +0500

    second commit

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    int main=0;
+    int main=77;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
